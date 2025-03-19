f06fafdb7e2ccd495160d0ddb3d477126230ef33
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-03-19 18:15:56 +0000

    Adjusts CI and adds A11y issue component

---
+++ src/components/Auth/AuthForm.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { expect, userEvent, within } from '@storybook/test'
+
+import { LoginForm } from './AuthForm'
+
+const meta: Meta<typeof LoginForm> = {
+  component: LoginForm,
+  title: 'Components/AuthForm',
+}
+
+export default meta
+type Story = StoryObj<typeof LoginForm>
+
+export const Default: Story = {}
+
+export const WithEmailFilled: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const emailInput = canvas.getByLabelText('Email')
+    await userEvent.type(emailInput, 'test@example.com')
+  },
+}
+
+export const WithPasswordFilled: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const passwordInput = canvas.getByLabelText('Password')
+    await userEvent.type(passwordInput, 'password123')
+  },
+}
+
